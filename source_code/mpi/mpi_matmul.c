d1f0cd7fe920f4a195abccf972a61a5a6f352348
B S K J Perera <it23223394@my.sliit.lk>
2025-12-05 19:18:59 +0530

    Add

--- source_code/mpi/mpi_matmul.c
+++ source_code/mpi/mpi_matmul.c
@@ -15,9 +15,11 @@ int main(int argc, char* argv[]) {
     double *A = NULL;
     double *B = (double*) malloc(N*N*sizeof(double));
     double *C_local = (double*) malloc(N*N/size * sizeof(double));
+    double *C = NULL;
 
     if (rank == 0) {
         A = (double*) malloc(N*N*sizeof(double));
+        C = (double*) malloc(N*N*sizeof(double));
         for (int i = 0; i < N*N; i++) {
             A[i] = rand() % 10;
             B[i] = rand() % 10;
@@ -39,9 +41,11 @@ int main(int argc, char* argv[]) {
         }
 
     end = MPI_Wtime();
+    MPI_Gather(C_local, N*N/size, MPI_DOUBLE, C, N*N/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0)
         printf("Processes: %d | Time: %.6f sec\n", size, end - start);
+    if (rank == 0 && C) free(C);
 
     MPI_Finalize();
     return 0;
